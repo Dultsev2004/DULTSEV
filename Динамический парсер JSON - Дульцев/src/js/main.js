ab6328a33ccfe720e5a092813df48361ef1abcab
Дульцев Сергей Иванович <gr702_dusiv@ttit.local>
2023-10-26 16:34:46 +0700

    Many fixes

--- Динамический парсер JSON - Дульцев/src/js/main.js
+++ Динамический парсер JSON - Дульцев/src/js/main.js
@@ -1,140 +1,16 @@
-//Добавление постов
-function addPost(){
-    const form = document.createElement("form");
-    form.classList.add(".form-post")
-    form.attributes('name', addPostForm.name);
+function readJSON(input) {
+    let file = input.files[0];
+    let readerFile = new FileReader();
+    readerFile.readAsText(file);
 
-    addPostForm.fields.forEach(fields => {
-        const label = document.createElement("label");
-        label.classList.add(".form-label")
-        label.textContent = fields.lable;
+    readerFile.onload = function() {
+        const string = readerFile.result;
+        const json = JSON.parse(string)
 
-        const input = document.createElement('input');
-        input.classList.add(".form-input")
-        for(let inputProperty in fields.input){
-            input.setAttribute(inputProperty, fields.input[inputProperty]) 
-        }
-
-        label.appendChild("label");
-        input.appendChild("input");
-    });
-
-    addPostForm.references.forEach(references => {
-        const input = document.createElement("input");
-        for(let inputProperty in fields.input){
-            input.setAttribute(inputProperty, references.input[inputProperty])
-        }
-
-        const referenceLink = document.createElement("a");
-        referenceLink.href = reference.ref;
-        referenceLink.textContent = reference.text;
-        if (reference.input.required) {
-            input.required = true;
-        }
-        if (reference.input.checked === "true") {
-            input.checked = true;
-        }
-
-        if (reference.text) {
-            const referenceLink = document.createElement("a");
-            referenceLink.href = reference.ref;
-            referenceLink.textContent = reference.text;
-            form.appendChild(input);
-            form.appendChild(referenceLink);
-        } else {
-            form.appendChild(input);
-        }
-    })
-
-    addPostForm.buttons.forEach(buttons => {
-        const button = document.createElement("button");
-        button.setAttribute("submit", "submit");
-        input.classList.add(".btn-default");
-    })
-
-    renderContent(addPost);
+        buildForm(json);
+    };
 }
 
-//Цветовая схема
-function colorSheme(colorSchemeForm){
-    const form = document.createElement("form");
-    form.classList.add(".form-color")
-    form.attributes('name', colorSchemeForm.name);
+function buildForm(json){
 
-    colorSchemeForm.fields.forEach(field => {
-        const label = document.createElement("label");
-        label.classList.add(".form-lable")
-        label.textContent = fields.lable;
-
-        const input = document.createElement('input');
-        input.classList.add(".form-input")
-        for(let inputProperty in fields.input){
-            input.setAttribute(inputProperty, fields.input[inputProperty])
-        }
-
-        lable.appendChild("label");
-        input.appendChild("input");
-    });
-
-    renderContent(colorSheme);
-}
-
-
-//Интервью
-function interview(interviewForm){
-    const form = document.createElement("form");
-    form.classList.add(".form-post")
-    form.attributes('name', addPostForm.name);
-
-    interviewForm.fields.forEach(fields => {
-        const label = document.createElement("label");
-        label.classList.add(".form-lable")
-        label.textContent = fields.lable;
-
-        const input = document.createElement('input');
-        input.classList.add(".form-input")
-        for(let inputProperty in fields.input){
-            input.setAttribute(inputProperty, fields.input[inputProperty]) 
-        }
-
-        lable.appendChild("lable");
-        input.appendChild("input");
-    });
-
-    interviewForm.references.forEach(references => {
-        const input = document.createElement("input");
-        for(let inputProperty in fields.input){
-            input.setAttribute(inputProperty, references.input[inputProperty])
-        }
-
-        const referenceLink = document.createElement("a");
-        referenceLink.href = reference.ref;
-        referenceLink.textContent = reference.text;
-
-        input.appendChild("input");
-        form.appendChild(referenceLink);
-    })
-
-    interviewForm.buttons.forEach(buttons => {
-        const button = document.createElement("button")
-        input.classList.add(".btn-default")
-    })
-
-    renderContent(interview);
-}
-
-
-//Авторизация
-function loginIn(signInForm){
-
-}
-
-function register(signUpForm){
-    
-}
-
-//Рендер в main
-function renderContent(content){
-    const main = document.querySelector(".main")
-    main.insertAdjacentHTML("before", content);
-}
+};
